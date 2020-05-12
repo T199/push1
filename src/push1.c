0a09a63d59999469f07b43dac7ca8ffa628091ed
T199 <tirupathitwo@gmail.com>
2020-05-12 19:28:30 +0530

    echo_first_commit

---
+++ src/push1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	puts("this is my first commit");
+	/* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+
+}
