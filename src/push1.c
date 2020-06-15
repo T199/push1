a615cb46ad8e4ff5460bc9846027d778e9a3cd1a
T199 <tirupathitwo@gmail.com>
2020-06-15 15:47:11 +0530

    timestorm-5.3.6-commits

--- src/push1.c
+++ src/push1.c
@@ -3,7 +3,7 @@
 
 int main(void) {
 	puts("!!!Hello World!!!");
-	puts("this is my first commit");
+	puts("this is my first commit in 5.3.6");
 	/* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 
